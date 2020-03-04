3ef2de9fc9753ea971f02d4319cd9f231a7e28cb
曾小健 <2119516028@qq.com>
2020-03-05 03:13:07 +0800

    Add files via upload

---
+++ keras-lenet5.py
@@ -0,0 +1,64 @@
+from keras.datasets import mnist
+from keras.utils import np_utils
+# Load dataset as train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Set numeric type to float32 from uint8
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Normalize value to [0, 1]
+x_train /= 255
+x_test /= 255
+
+# Transform lables to one-hot encoding
+y_train = np_utils.to_categorical(y_train, 10)
+y_test = np_utils.to_categorical(y_test, 10)
+
+# Reshape the dataset into 4D array
+x_train = x_train.reshape(x_train.shape[0], 28,28,1)
+x_test = x_test.reshape(x_test.shape[0], 28,28,1)
+
+
+from keras.models import Sequential
+from keras import models, layers
+import keras
+#Instantiate an empty model
+model = Sequential()
+
+# C1 Convolutional Layer
+model.add(layers.Conv2D(6, kernel_size=(5, 5), 
+                        strides=(1, 1), activation='relu', 
+                        input_shape=(28,28,1), padding="same"))
+
+# S2 Pooling Layer
+model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding='valid'))
+
+# C3 Convolutional Layer
+model.add(layers.Conv2D(16, kernel_size=(5, 5), strides=(1, 1), 
+                        activation='relu', padding='valid'))
+
+# S4 Pooling Layer
+model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+
+# C5 Fully Connected Convolutional Layer
+model.add(layers.Conv2D(120, kernel_size=(5, 5), strides=(1, 1), 
+                        activation='relu', padding='valid'))
+#Flatten the CNN output so that we can connect it with fully connected layers
+model.add(layers.Flatten())
+
+# FC6 Fully Connected Layer
+model.add(layers.Dense(84, activation='relu'))
+
+#Output Layer with softmax activation
+model.add(layers.Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer='SGD', metrics=["accuracy"])
+
+
+hist = model.fit(x=x_train,y=y_train, epochs=10, batch_size=128, validation_data=(x_test, y_test), verbose=1)
+
+
+test_score = model.evaluate(x_test, y_test)
+print("Test loss {:.4f}, accuracy {:.2f}%".format(test_score[0], test_score[1] * 100))
